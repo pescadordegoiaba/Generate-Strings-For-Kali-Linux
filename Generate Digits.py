8baff24ef0645b50b6f060e0e7de7640d2ca286c
Opalao tunado Turbo Ultra Economico <96215169+pescadordegoiaba@users.noreply.github.com>
2023-07-30 21:59:51 -0400

    Update Generate Digits.py

--- Generate Digits.py
+++ Generate Digits.py
@@ -2,39 +2,73 @@ import os
 import random
 import string
 import time
+import threading
 
-def generate_random_string(length):
+def generate_random_string_v2(length):
     characters = string.ascii_letters + string.digits
+    code_type = random.randint(1, 4)
+
+    if code_type == 1:
+        characters = string.ascii_lowercase
+    elif code_type == 2:
+        characters = string.ascii_uppercase
+    elif code_type == 3:
+        characters = string.ascii_letters
+
     return ''.join(random.choice(characters) for _ in range(length))
 
 def create_directory_if_not_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def check_existing_code(size, code, destination_path):
+    directory_name = f"{size}strings"
+    file_path = os.path.join(destination_path, directory_name, f"{size}strings.txt")
+
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            for line in file:
+                if line.strip() == code:
+                    return True
+
+    return False
+
 def save_to_file(size, code, destination_path):
     directory_name = f"{size}strings"
     create_directory_if_not_exists(os.path.join(destination_path, directory_name))
 
     filename = os.path.join(destination_path, directory_name, f"{size}strings.txt")
-    with open(filename, 'a') as file:
-        file.write(code + "\n")
 
-    print(f"generate {size} strings: {code}")
-    print(f"File saved in {filename}")
+    if not check_existing_code(size, code, destination_path):
+        with open(filename, 'a') as file:
+            file.write(code + "\n")
+
+        print(f"generate {size} strings: {code}")
+        print(f"File saved in {filename}")
+    else:
+        print(f"Code {code} already exists. Generating a new one.")
+
+def generate_and_save(sizes, destination_path):
+    while True:
+        size = random.choice(sizes)
+        code = generate_random_string_v2(size)
+        save_to_file(size, code, destination_path)
+        time.sleep(1)
 
 def main():
     destination_path = "/home/kali/Desktop/Generated-Digits/"
+    num_threads = int(input("Enter the number of threads: "))
 
     sizes = [4, 6, 8, 16]
 
-    while True:
-        create_directory_if_not_exists(destination_path)
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=generate_and_save, args=(sizes, destination_path))
+        threads.append(thread)
+        thread.start()
 
-        size = random.choice(sizes)
-        code = generate_random_string(size)
-        save_to_file(size, code, destination_path)
-
-        time.sleep(1)
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
     main()
